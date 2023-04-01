3e5bb66b8951b15fde0d6c1b3c075542f945a90e
Philip Osei Agyen <agyinp@gmail.com>
2023-04-01 14:36:44 -0700

    edited 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,29 +4,24 @@
  *
  *@n: parameter to be printed
  *
- * Return: 1, 0, -1 if it alphabet and 0 otherwise
- *
+ * Return: 1 is greater than zero. 0 is zero. 
+ * -1 is less than zero.
  */
 int print_sign(int n)
 {
 	if (n > 0)
-	{
-		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
-		return (1);
-	}
-	else if (n == 0)
-	{	_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
-		return (-1);
-	}
+        {
+                _putchar(43)
+                return (1);
+        }
+        else if (n < 0)
+        {       _putchar(45);
+                return (-1);
+        }
+        else
+        {
+                _putchar(48);
+                return (0);
+        }
+	_putchar('\n')
 }
