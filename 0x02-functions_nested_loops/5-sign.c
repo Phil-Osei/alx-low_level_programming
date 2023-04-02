484d8fe47e59771fb2917dc37571af8572150bfc
Philip Osei Agyen <agyinp@gmail.com>
2023-04-02 11:53:06 -0700

    Updated 5-sign.c to fix bug of non-void function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,24 +4,24 @@
  *
  *@n: parameter to be printed
  *
- * Return: 1 is greater than zero. 0 is zero. 
+ * Return: 1 is greater than zero. 0 is zero.
  * -1 is less than zero.
  */
 int print_sign(int n)
 {
 	if (n > 0)
-        {
-                _putchar(43)
-                return (1);
-        }
-        else if (n < 0)
-        {       _putchar(45);
-                return (-1);
-        }
-        else
-        {
-                _putchar(48);
-                return (0);
-        }
-	_putchar('\n')
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
 }
