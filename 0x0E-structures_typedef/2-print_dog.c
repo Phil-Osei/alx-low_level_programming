9ce76f25ae7e5e815cbbdf8be12e1e0683213f35
Philip Osei Agyen <agyinp@gmail.com>
2023-04-18 22:24:59 +0000

    debug for correct output in ALX

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		d->name = "(nil)";
 
-	printf("Name of dog is: %s\n", d->name);
-	printf("Age is: %f\n", d->age);
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 }
